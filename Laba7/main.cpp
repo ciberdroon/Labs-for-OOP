f97429b9075fc57a9d487ddcd2dc262ac5e342d5
ciberdroon <73558303+ciberdroon@users.noreply.github.com>
2020-10-28 16:24:55 +0300

    Lab7

--- Laba7/main.cpp
+++ Laba7/main.cpp
@@ -1,77 +1,27 @@
 #include "stdio.h"
 #include "iostream"
 
-int main()
-{
-    int i,j,n=3;
+using namespace std;
 
-    int **a=new int*[n];
-    for (i=0; i<n; i++)
-    {
-        a[i]=new int[n];
-    }
+class Matrix {
+public:
+    static const int length = 3;
+    int mat[length][length];
 
-    int **b=new int*[n];
-    for (i=0; i<n; i++)
-    {
-        b[i]=new int[n];
-    }
-
-
-    printf("Enter elements matrix\n");
-    for (i=0;i<3;i++)
-    for (j=0;j<3;j++)
-    {
-        scanf("%d",&a[i][j]);
-
-    }
-
-    printf("Matrix\n");
-    for (i=0;i<3;i++)
-    {
-        for (j=0;j<3;j++)
-        printf("%d ",a[i][j]);
-        printf("\n");
-    }
-
-    for (i=0;i<3;i++)
-    {
-        for (j=0;j<3;j++)
-        b[i][j]=a[j][i];
-    }
-
-    printf("Transpore massiv\n");
-    for (i=0;i<3;i++)
-    {
-        for (j=0;j<3;j++)
-        printf("%d ",b[i][j]);
-        printf("\n");
-    }
-
-    printf("\n");
-    printf("\n");
-
-
-    for (i=0;i<3;i++)
-    {
-        for (j=0;j<3;j++)
-        printf("%d ",a[i][j]);
-        printf("\n");
-    }
-
-    int flag=0;
-    for (i=0; i<n; i++)
-    {
-        for(j=0; j<n; j++)
-        {
-            if (i!=n  && a[i][j]!=b[i][j])
-                flag=1;
+    Matrix() {
+        srand(time(NULL));
+        for (int i = 0; i < length; ++i) {
+            for (int j = 0; j < length; ++j) {
+                mat[i][j] = rand() % 90 + 10;
+            }
         }
     }
 
-    if (flag==1)
-        printf ("Matrix is not symmetrical");
-    else
-        printf ("Matrix is symmetrical");
-
-}
+    void print(Matrix mat) {
+        for (int i = 0; i < length; ++i) {
+            for (int j = 0; j < length; ++j) {
+                cout << mat.mat[i][j] << endl;
+            }
+        }
+    }
+};
