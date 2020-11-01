f5a06e144ce7df81664437e0928680f65bc79bb2
ciberdroon <73558303+ciberdroon@users.noreply.github.com>
2020-11-01 21:32:34 +0300

    памагите

--- laba6/main.cpp
+++ laba6/main.cpp
@@ -1,29 +1,63 @@
-#include <iostream>
+#include "iostream"
 
 using namespace std;
 
 class IVector {
 private:
-    static const int length = 3;
-    int vector[length];
+    int vector[3];
 public:
-    IVector() {
-        srand(time(NULL));
-        for (int i = 0; i < length; ++i) {
-            if (vector[length] == vector[length] * vector[length]) {
-                vector[length] = rand() % 10 + 10;
-            }
+    IVector operator++(int vec) {
+        for (int i = 0; i < 3; ++i) {
+            this->vector[i] = vector[i] * vector[i];
+        }
+    }
+
+    IVector operator--(int vec) {
+        for (int i = 0; i < 3; ++i) {
+            this->vector[i] = vector[i] - vector[i + 1];
+        }
+    }
+
+    IVector operator+(int vec) {
+        for (int i = 0; i < 3; ++i) {
+            this->vector[i]=vector[i] + vector[i];
+        }
+    }
+    bool operator<(IVector &vec, IVector &vec2){
+        for (int i = 0; i < 3; ++i) {
 
         }
     }
 
-    void print(IVector vec) {
-        for (int i = 0; i < length; ++i) {
-            cout << vec.vector [length] << endl;
-        }
-    }
 };
 
-int main() {
-    IVector vec;
-}
+friend IVector operator--(IVector vec) {
+    for (int i = 0; i < 3; ++i) {
+        this->vector[i] = vector[i] - vector[i + 1];
+    }
+
+    bool operator==(MyInt &myInt1, MyInt &myInt2) {
+        return myInt1.value == myInt2.value;
+    }
+
+    MyInt & operator
+    — (
+            MyInt & myInt
+    )
+    {
+        myInt.value -= 3;
+        return
+                myInt;
+    }
+
+    int main() {
+        MyInt myInt(22);
+        cout « !myInt « endl;
+        MyInt
+        myInt2 = —myInt;
+        myInt.print();
+        MyInt myInt1 = myInt + 7;
+        myInt.print();
+        cout « (myInt == myInt2) « endl;
+        return 0;
+    }
